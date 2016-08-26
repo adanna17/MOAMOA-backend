cf9d4b4915c401008cd46107c12c1f746f6d42c5
KIM JUNG MIN <ctl_anna17@naver.com>
2016-08-26 10:57:46 +0900

    유저 d. moa id중복 확인

--- moamoa/routes/user.js
+++ moamoa/routes/user.js
@@ -27,5 +27,28 @@ router.post('/login',function(req,res){
 });
 //기존 사용자인지 신규 가입자인지 확인
 
+router.post('/register/repeat',function(req,res){
+
+  var checkId = req.body.user_moa_id;
+  console.log(checkId);
+
+  var sql = 'SELECT user_moa_id FROM user';
+  db.query(sql, function(err, result){
+    if (err) {
+      console.log(err);
+      res.status(500);
+    } else {
+      for (var i = 0; i < result.length; i++) {
+        if (checkId == result[i].user_moa_id) {
+          ServerResult.bResult = true;
+        }
+      }
+    }
+  });
+
+  res.json(ServerResult);
+
+});
+//회원가입시 아이디 중복확인
 
 module.exports = router;
