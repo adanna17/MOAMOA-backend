3439b437d6130766bd9a9a2b2e59ccb46c8010f7
KIM JUNG MIN <ctl_anna17@naver.com>
2016-08-26 16:47:26 +0900

    ServerResult boolean이 아니라 string으로 변경

--- moamoa/routes/user.js
+++ moamoa/routes/user.js
@@ -3,27 +3,29 @@ var router = express.Router();
 var db = require('../util/db.js');
 
 var ServerResult = {
-  bResult : false
+  resultStatus : 'default'
 };
 
 router.post('/login',function(req,res){
   var checkUser = req.body.user_kaokao_id;
 
-  var sql = 'SELECT user_kaokao_id FROM user';
-  db.query(sql, function(err, result){
+  var sql = 'SELECT user_kaokao_id, user_moa_id  FROM user WHERE user_kaokao_id = ?';
+  db.query(sql, checkUser, function(err, result){
     if (err) {
       res.status(500);
-      ServerResult.bResult = false;
+      ServerResult.resultStatus = 'error';
+      //오류 발생
     } else {
-      for (var i = 0; i < result.length; i++) {
-        if (checkUser == result[i].user_kakao_id) {
-          ServerResult.bResult = true;
-        }
+      if (result[0] == null) {
+        ServerResult.resultStatus = 'new';
+        // 신규 사용자일 경우
+      }else{
+        ServerResult.resultStatus = result[0].user_moa_id
+        // 기존 사용자일 경우
       }
-      res.json(ServerResult);
     }
+    res.json(ServerResult);
   });
-
 });
 //기존 사용자인지 신규 가입자인지 확인
 
@@ -31,23 +33,23 @@ router.post('/register/repeat',function(req,res){
 
   var checkId = req.body.user_moa_id;
 
-  var sql = 'SELECT user_moa_id FROM user';
-  db.query(sql, function(err, result){
+  var sql = 'SELECT user_moa_id FROM user WHERE user_moa_id = ? ';
+  db.query(sql, checkId, function(err, result){
     if (err) {
-      console.log(err);
       res.status(500);
-      ServerResult.bResult = false;
+      ServerResult.resultStatus = 'error';
+      //오류 발생
     } else {
-      for (var i = 0; i < result.length; i++) {
-        if (checkId == result[i].user_moa_id) {
-          ServerResult.bResult = true;
-        }
+      if (result[0] == null) {
+        ServerResult.resultStatus = 'success';
+        // 중복 성공(사용할 수 있는 아이디)
+      }else{
+        ServerResult.resultStatus = 'fail';
+        // 중복 실패(이미 사용중인 아이디)
       }
     }
+    res.json(ServerResult);
   });
-
-  res.json(ServerResult);
-
 });
 //회원가입시 아이디 중복확인
 
@@ -63,16 +65,14 @@ router.post('/register/submit',function(req,res){
   var sql = 'INSERT INTO user SET ?';
   db.query(sql, user, function(err, result){
     if (err) {
-      console.log(err);
       res.status(500);
-      ServerResult.bResult = false;
+      ServerResult.resultStatus = 'error';
+      //오류 발생
     } else {
-      ServerResult.bResult = true;
+      ServerResult.resultStatus = 'success';
     }
+    res.json(ServerResult);
   });
-
-  res.json(ServerResult);
-
 });
 //회원가입
 
